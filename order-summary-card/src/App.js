4aab28e86a4e4c870a311a3faab8b3c93f60e0ae
Yulany Munevar <andra_852@hotmail.com>
2022-10-26 14:06:23 -0500

    React project2

---
+++ order-summary-card/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import OrderSummary from './Components/OrderSummary/OrderSummary'; 
+
+function App() {
+  return (
+    <div className="App">
+      <OrderSummary/>
+    </div>
+  );
+}
+
+export default App;
---
+++ order-summary-card/src/Components/OrderSummary/OrderSummary.jsx
@@ -0,0 +1,44 @@
+import './styles.css'
+import hero from './images/hero.svg';
+import music from './images/music.svg'
+
+
+function OrderSummary(){
+    return (
+
+       < div id='container'> 
+            <header className='principal_image'>
+                <img id='hero'src={hero} />
+                <h1>Order Summary</h1>
+                <p>You can now listen to millions of song,
+                    audiobooks, and podcast on any device
+                    anywhere you like!
+                </p>
+            </header>
+            <div className="plan">
+                <div className="music">
+                    <img src={music} />
+                </div>
+                <div className="annualPlan">
+                    <p id='plan'>Annual Plan</p>
+                    <p id='price'> $ 59.99/year</p>
+                </div>
+
+                <div id='change'>
+                    <button id='ch'>Change</button>
+                </div>
+            </div>
+
+            <footer id='buttoms'>
+                <button id='btm1'>Proceed to Payment</button>
+                <button id='btm2'>Cancel Order</button>
+
+            </footer>
+
+
+       </div>
+
+    )
+}
+
+export default OrderSummary
